57267a30b6613e1c4c93f9e39c6c98d415e9bfe2
cory <corybsa@gmail.com>
2020-05-29 17:33:56 -0400

    TileEditor is now used to edit an actual tile.

--- src/app/tile-editor/tile-editor.component.ts
+++ src/app/tile-editor/tile-editor.component.ts
@@ -1,30 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {TileComponent} from '../tile/tile.component';
 
 @Component({
   selector: 'app-tile-editor',
   templateUrl: './tile-editor.component.html',
   styleUrls: ['./tile-editor.component.css']
 })
-export class TileEditorComponent implements OnInit {
-  scale: number;
-  rows: number;
-  cols: number;
+export class TileEditorComponent implements OnInit, OnChanges {
+  @Input('tile') sourceTile: TileComponent;
 
-  constructor() {
-    this.scale = 8;
-    this.rows = 8;
-    this.cols = 16;
-  }
+  @ViewChild(TileComponent) tile: TileComponent;
+
+  scale = 8;
+
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  changeRows($event) {
-    this.rows = +$event.target.value;
+  ngOnChanges(changes: SimpleChanges) {
+    if(this.tile) {
+      this.tile.pixels = this.sourceTile.pixels;
+    }
   }
 
-  changeCols($event) {
-    this.cols = +$event.target.value;
+  formatLabel(value: number) {
+    return `${value}x`;
   }
 
 }
