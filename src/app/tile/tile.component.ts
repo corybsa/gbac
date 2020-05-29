636559221a199120815335b16ccb2abb3a9134f5
cory <corybsa@gmail.com>
2020-05-29 17:36:33 -0400

    Add option to highlight pixel on mouse hover.

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -7,6 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class TileComponent implements OnInit {
   @Input() scale: number;
+  @Input() editing = false;
 
   pixels = [];
 
@@ -15,8 +16,8 @@ export class TileComponent implements OnInit {
 
   ngOnInit(): void {
     for(let i = 0; i < 64; i++) {
-      // this.pixels.push({ color: Math.floor(Math.random() * 4) });
-      this.pixels.push({ color: 3 });
+      // this.pixels.push(Math.floor(Math.random() * 4));
+      this.pixels.push(3);
     }
   }
 }
